99a97371906099c6fb07b8d12470bb79e68af7d3
Rakesh Viyak <rakeshviyak@gmail.com>
2015-02-08 04:00:31 +0800

    add calendar

---
+++ calendar.php
@@ -0,0 +1,22 @@
+<?php include('base_top.html'); ?>
+
+    <section style="margin-top:30px">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Calendar</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            
+            <div class="row col-lg-offset-1" style="padding-left:10px">
+                <iframe class="notebook" src="https://www.google.com/calendar/embed?title=The%20Migrant%20Network&amp;height=700&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=ddrcgvmmuuh76011o63he4s5b8%40group.calendar.google.com&amp;color=%235F6B02&amp;ctz=Asia%2FSingapore" style=" border-width:0 " width="900" height="700" frameborder="0" scrolling="no"></iframe>
+
+                <iframe class="tablet" src="https://www.google.com/calendar/embed?title=The%20Migrant%20Network&amp;height=500&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=ddrcgvmmuuh76011o63he4s5b8%40group.calendar.google.com&amp;color=%235F6B02&amp;ctz=Asia%2FSingapore" style=" border-width:0 " width="700" height="500" frameborder="0" scrolling="no"></iframe>
+
+                <iframe class="phone" src="https://www.google.com/calendar/embed?title=The%20Migrant%20Network&amp;height=200&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=ddrcgvmmuuh76011o63he4s5b8%40group.calendar.google.com&amp;color=%235F6B02&amp;ctz=Asia%2FSingapore" style=" border-width:0 " width="330" height="200" frameborder="0" scrolling="no"></iframe>
+            </div>
+        </div>
+    </section>
+
+<?php include('base_bottom.html'); ?>
